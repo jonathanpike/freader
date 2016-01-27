ae64519eae3dd153a2ce7491e5c7b2ed412dc1b6
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-01-27 19:49:56 +0000

    Add validations to user model, add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ActiveRecord::Base
   has_many :subscriptions
   has_many :sites, through: :subscriptions
   
+  before_save { self.email = email.downcase }
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  VALID_EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\z/i
+  validates :email, presence: true, length: { maximum: 255 }, 
+  format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
+  validates :password, presence: true, length: { minimum: 8 }
+  
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,81 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com", 
+            password: "bobdylan", password_confirmation: "bobdylan")
+  end
+  
+  test "should be valid" do 
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = "      "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "      "
+    assert_not @user.valid?
+  end
+  
+  test "name length should be < 50" do
+    @user.name = "A" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email length should be < 255" do
+    @user.email = "A" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email should be valid" do
+    valid_addresses = %w(prettyandsimple@example.com very.common@example.com 
+                        disposable.style.email.with+symbol@example.com 
+                        other.email-with-dash@example.com)
+    valid_addresses.each do |valid|
+      @user.email = valid 
+      assert @user.valid?, "#{valid.inspect} must be a valid e-mail address"
+    end
+    
+    invalid_addresses = %w(Abc.example.com A@b@c@example.com blah@example 
+                        invalid@address,com)
+    invalid_addresses.each do |invalid|
+      @user.email = invalid
+      assert_not @user.valid?, "#{invalid.inspect} must be a valid e-mail address"
+    end
+  end
+  
+  test "email must be unique" do 
+    @user.save
+    user2 = User.new(name: "Example User", email: "user@example.com", 
+            password: "bobdylan", password_confirmation: "bobdylan")
+    
+    assert_not user2.valid?
+    
+    user2.email = user2.email.upcase
+    
+    assert_not user2.valid?
+  end
+  
+  test "email is saved in downcase" do
+    user2 = User.new(name: "Example User", email: "USER@EXAMPLE.COM",
+            password: "bobdylan", password_confirmation: "bobdylan")
+    user2.save
+    
+    assert_equal user2.email, "user@example.com"
+  end
+  
+  test "password should not be blank" do 
+    @user.password = "    "
+    
+    assert_not @user.valid?
+  end
+  
+  test "password length should be > 8" do 
+    @user.password = "bob"
+    
+    assert_not @user.valid?
+  end
 end
