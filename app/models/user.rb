3ea9369c2ef7e042a24d8c3baaf469416c695266
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-01-28 17:12:55 +0000

    Implement destroy action for session, add helper for log out, add integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,10 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 8 }
 
   has_secure_password
+  
+  def User.digest(password)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(password, cost: cost)
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup 
+    @user = users(:cooper)
+  end
+  
   test "invalid login" do
     get login_path
     post login_path, session: { email: "blah@blah.com",
@@ -9,5 +13,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_equal flash[:alert], "Incorrect username and password"
     get root_path
     assert_nil flash[:alert]
+  end
+  
+  test "log in and log out" do 
+    get login_path 
+    post login_path, session: { email: @user.email, password: "password" }
+    assert logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_template 'static_pages/index'
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_path
   end
 end
