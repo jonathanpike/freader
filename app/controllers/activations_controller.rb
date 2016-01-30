26117cd285c6257d7caa2fe4fbdbe66d09b7b974
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-01-30 19:07:58 +0000

    Move redirect out of conditional

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -2,13 +2,12 @@ class ActivationsController < ApplicationController
   def edit
     @user = User.find_by(email: params[:email])
     if @user && !@user.activated? && @user.authenticated?(:activation, params[:id])
-      @user.activate 
+      @user.activate
       log_in @user
       flash[:notice] = "Thanks for activating your account!"
-      redirect_to root_path
-    else 
+    else
       flash[:alert] = "Incorrect activation token."
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 end
