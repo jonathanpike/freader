7405e0680f9f9ee7ba373f51c6b124ec538bcc21
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-01-30 19:09:03 +0000

    Move activation conditional out of create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # rubocop:disable Metrics/AbcSize
   def create
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
-      if @user.activated?
-        log_in @user
-        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-        redirect_to root_path
-      else 
-        flash[:alert] = "Please check your e-mail to activate your account"
-        redirect_to root_path
-      end
+      activated_log_in(@user)
+      redirect_to root_path
     else
       flash.now[:alert] = "Incorrect username and password"
       render 'new'
@@ -23,5 +16,14 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_path
+  end
+
+  private
+
+  def activated_log_in(user)
+    message = "Please check your e-mail to activate your account"
+    return flash[:alert] = message unless user.activated?
+    log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   end
 end
