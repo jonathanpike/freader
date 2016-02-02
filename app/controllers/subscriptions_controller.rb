c6976ff06019767a55c9a7ac2bf85e4eb0809e48
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-02 19:54:57 +0000

    Add new and create actions to subscriptions controller, tests

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,17 +1,30 @@
 class SubscriptionsController < ApplicationController
   before_action :logged_in_user?
 
+  def index
+  
+  end
+
   def new
     @subscription = Subscription.new
   end
 
   def create
     @subscription = Subscription.new(subscription_params)
+    if @subscription.save
+      @subscription.add_feed_url
+      @subscription.site_exists? 
+      flash[:notice] = "Feed successfully added"
+      redirect_to root_path
+    else 
+      flash[:alert] = "Something went wrong.  Please try again."
+      render 'new'
+    end
   end
 
   private
 
   def subscription_params
-    params.require(:subscription).permit(:url)
+    params.require(:subscription).permit(:url, :user_id)
   end
 end
