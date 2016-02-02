389bb0b46c78f76766b0706957ef091206ac36ef
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-02 19:55:43 +0000

    Add methods for parsing feed url to subscription model, tests

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,35 @@
+require 'feedbag'
+
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :site
+  
+  validates :url, presence: true
+  
+  def site_exists?
+    if Site.exists?(url: self.url)
+      update_attribute(:site_id, 
+                       Site.find_by(url: self.url).id)
+    else 
+      new_site = Site.create(url: self.url, 
+                             feed_url: self.parse_feed_url)
+      update_attribute(:site_id, new_site.id)
+    end
+  end 
+  
+  def add_feed_url 
+    update_attribute(:feed_url, parse_feed_url)
+  end
+  
+  def parse_feed_url
+    url = sanitize_url(self.url)
+    Feedbag.find(url)[0]
+  end
+  
+  private
+  
+  def sanitize_url(url)
+    return url if url.include?("https://") || url.include?("http://")
+    url.prepend("http://")
+  end
 end
--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class SubscriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @rss = Subscription.create(url: "http://jonathanpike.net")
+    @atom = Subscription.create(url: "http://daringfireball.net")
+    @site = Site.create(title: "Daring Fireball", 
+                        author: "John Gruber",
+                        url: "http://daringfireball.net",
+                        feed_url: "http://daringfireball.net/feeds/main")
+  end 
+  
+  test "parse feed works with valid rss address" do 
+    actual = @rss.parse_feed_url
+    expected = "http://www.jonathanpike.net/feed.xml"
+    assert_equal expected, actual
+  end 
+  
+  test "parse feed works with valid atom address" do 
+    actual = @atom.parse_feed_url
+    expected = "http://daringfireball.net/feeds/main"
+    assert_equal expected, actual
+  end
+  
+  test "feed url is present" do 
+    @rss.add_feed_url 
+    actual = @rss.feed_url
+    expected = "http://www.jonathanpike.net/feed.xml"
+    assert_equal expected, actual
+  end 
+  
+  test "site_exists? with non-existant site" do 
+    assert_difference 'Site.count' do 
+      @rss.site_exists? 
+    end
+    site = Site.find_by(url: @rss.url)
+    assert_equal @rss.site_id, site.id
+  end
+  
+  test "site_exists? with existant site" do 
+    assert_no_difference 'Site.count' do 
+      @atom.site_exists?
+    end
+    assert_equal @atom.site_id, @site.id
+  end
 end
