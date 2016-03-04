a649f7416618d0992b8ede244d4ab61646325bb9
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-04 20:34:09 +0000

    Add routes for subscriptions index, sites show

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   
   resources :subscriptions, only: [:new, :create]
   
+  get 'yourdigest' => 'subscriptions#index'
+  
+  resources :sites, only: [:show]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
