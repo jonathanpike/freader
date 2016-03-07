d255cdf0217a3c2e50c09e040e982a844c4d5e24
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-07 21:15:56 +0000

    Add route for sidekiq web interface

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
   
   resources :sites, only: [:show]
   
+  # Sidekiq
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
