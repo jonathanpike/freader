f1e600a85659604a544193ded8d5f5d41a6be8ae
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-07 21:17:17 +0000

    Add test for show action in sites controller

--- test/controllers/sites_controller_test.rb
+++ test/controllers/sites_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SitesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @site = sites(:jp)
+  end
+  
+  test "should show" do 
+    get :show, id: @site.id
+    assert_response :success
+  end 
 end
