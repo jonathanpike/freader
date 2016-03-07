f782e7a45c73063f9f6442898b5a324025918bf5
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-07 21:20:38 +0000

    Fix rubocop errors in sites controller test

--- test/controllers/sites_controller_test.rb
+++ test/controllers/sites_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class SitesControllerTest < ActionController::TestCase
-  def setup 
+  def setup
     @site = sites(:jp)
   end
-  
-  test "should show" do 
+
+  test "should show" do
     get :show, id: @site.id
     assert_response :success
-  end 
+  end
 end
