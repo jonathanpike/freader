31e50ba2f60ea657d020120284cc612f3375b75d
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-07 21:14:24 +0000

    Change activerecord query to paginate

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,6 @@
 class SitesController < ApplicationController
   def show
     @site = Site.find(params[:id])
-    @articles = @site.articles.page(params[:page])
+    @articles = @site.articles.order("published desc").page(params[:page])
   end
 end
