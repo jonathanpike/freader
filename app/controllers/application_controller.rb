cfc8ef00b65afd8292835f227c4687aaf1a92049
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-14 20:13:05 +0000

    Add helper methods to application controller, use helper methods in partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  helper_method :new_subscription
+  helper_method :subscribed_sites
 
   # rubocop:disable Style/GuardClause
   def logged_in_user?
@@ -12,4 +15,12 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def new_subscription
+    Subscription.new
+  end 
+  
+  def subscribed_sites
+    current_user.sites.all.order("LOWER(title) asc")
+  end 
 end
