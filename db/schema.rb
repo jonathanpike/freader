81682b70e6dae0492ae744b569e411a3e3186e0a
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-16 14:44:04 +0000

    Add link column to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203171358) do
+ActiveRecord::Schema.define(version: 20160314204234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160203171358) do
     t.integer  "site_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "link"
   end
 
   add_index "articles", ["site_id"], name: "index_articles_on_site_id", using: :btree
