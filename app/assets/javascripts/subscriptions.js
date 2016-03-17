dd4bf7384d4c6ca2acf92d8db4f58418d16d2958
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-17 17:42:31 +0000

    Fix positioning and hiding of add new subscription popup

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,15 +1,28 @@
+var visible = $('.new_subscription_box').hasClass('hide');
+
 var ready = function() {
-  $('.new_subscription_button').on('click', function(e) { 
-    e.preventDefault();
-    if ($('.new_subscription_box').hasClass('visible')) {
-      $('.new_subscription_box').removeClass('visible');
-      $('.new_subscription_box').addClass('invisible');
-    } else {
-      $('.new_subscription_box').removeClass('invisible');
-      $('.new_subscription_box').addClass('visible');
-    }
-  });
   
+  $('.sidebar').on('click', '.new_subscription_button', function(e) { 
+    e.preventDefault();
+    
+    new Tether({
+      element: '.new_subscription_box',
+      target: '.new_subscription_button',
+      attachment: 'middle left'
+    });
+    
+    if (visible) {
+    	$('.new_subscription_box').fadeOut('fast',function(){
+        $('.new_subscription_box').addClass('hide')
+             .fadeIn(0); });
+     } else {
+      	$('.new_subscription_box').fadeOut(0,function(){
+        $('.new_subscription_box').removeClass('hide')
+             .fadeIn('fast'); });
+     }
+    visible = ! visible;
+  });
+
   $('body').on('click', '.select_all', function(e) {
     var cbxs = $('input[type="checkbox"]');
     cbxs.prop("checked", !cbxs.prop("checked"));
