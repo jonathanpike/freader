b54edac499c338cd2775ba3cfe72c199442cd0bd
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-23 16:02:34 +0000

    Moved instapaper.rb to app/models/

--- app/models/instapaper.rb
+++ app/models/instapaper.rb
@@ -3,43 +3,54 @@ require 'net/http'
 # Thank you to http://danknox.github.io/2013/01/27/using-rubys-native-nethttp-library/
 
 class Instapaper
-  def initialize(username, password)
-    @username = username
-    @password = password
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+
+  attr_reader :username, :password
+
+  def initialize
     uri = URI("https://www.instapaper.com/")
     @http = Net::HTTP.new(uri.host, uri.port)
-    @http.use_ssl = true  
-  end 
-  
+    @http.use_ssl = true
+  end
+
+  def create(username, password)
+    @username = username
+    @password = password
+  end
+
   def authenticate
     res = http_request("/api/authenticate")
-
     return true if res.code == "200"
     false
   end
-  
+
   def add(url)
-    res = http_request("/api/add", { url: url })
+    res = http_request("/api/add", url: url)
     return true if res.code == "201"
     false
-  end 
-  
-  private 
-  
+  end
+
+  def persisted?
+    false
+  end
+
+  private
+
   def http_request(path, params = {})
     if params.empty?
       req = Net::HTTP::Get.new(path)
-    else 
+    else
       full_path = encode_path_params(path, params)
       req = Net::HTTP::Get.new(full_path)
-    end 
+    end
     req.basic_auth @username, @password
-    
+
     @http.request(req)
-  end 
-  
+  end
+
   def encode_path_params(path, params)
     encoded = URI.encode_www_form(params)
     [path, encoded].join("?")
   end
-end 
+end
