60c50f1e2f61232e0a2aa63843aaef3350cd1359
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-03-24 17:22:42 +0000

    Remove tether, remove all code related to add subscription popup

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,49 +1,10 @@
-var visible = $('.new_subscription_box').hasClass('hide');
-
 var ready = function() {
-  
-  $('.sidebar').on('click', '.new_subscription_button', function(e) { 
-    e.preventDefault();
-    e.stopPropagation();
-    
-    new Tether({
-      element: '.new_subscription_box',
-      target: '.new_subscription_button',
-      attachment: 'middle left'
-    });
-    
-    if (visible) {
-    	$('.new_subscription_box').fadeOut('fast',function(){
-        $('.new_subscription_box').addClass('hide')
-             .fadeIn(0); });
-     } else {
-      	$('.new_subscription_box').fadeOut(0,function(){
-        $('.new_subscription_box').removeClass('hide')
-             .fadeIn('fast'); });
-     }
-    visible = ! visible;
-  });
-
-  // If new_subscription_box is clicked, don't let the 
-  // event bubble up to document (for function below)
-  $('.new_subscription_box').on('click', function(e) {
-      e.stopPropagation();
-  });
-
-  // Dismiss popup if document is clicked
-  $(document).on('click', function(e) {
-    if (visible) {
-    	$('.new_subscription_box').fadeOut('fast',function(){
-        $('.new_subscription_box').addClass('hide')
-             .fadeIn(0); });
-    }
-    visible = ! visible;
-  });
-
   $('body').on('click', '.select_all', function(e) {
     var cbxs = $('input[type="checkbox"]');
     cbxs.prop("checked", !cbxs.prop("checked"));
   });
+  
+  $.lockfixed(".my_subscriptions", {offset: {top: 10, bottom: 100}});
 };
 
 $(document).ready(ready);
