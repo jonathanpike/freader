9b11c79ca5e39be2dbd3d005ab323a88dce6cb8d
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-01 20:53:19 +0000

    Make navbar stick to top on scroll

---
+++ app/assets/javascripts/stickynavbar.js
@@ -0,0 +1,24 @@
+var ready = function() {
+    "use strict";
+
+    var y_pos = $('#navbar').offset().top;
+    var height = $('#navbar').height();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $('#navbar').addClass("navbar-fixed").animate({
+                top: 0
+            });
+        } else if (scrollTop <= y_pos) {
+            $('#navbar').removeClass("navbar-fixed").clearQueue().animate({
+                top: "-48px"
+            }, 0);
+        }
+    });
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
