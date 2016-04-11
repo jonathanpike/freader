661c9d7c42a27df1906ed47df0f5bcb444d2f34b
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-11 20:12:24 +0000

    Add show_errors helper for forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_errors(object, field_name)
+    if object.errors.any?
+      unless object.errors.full_messages_for(field_name).blank?
+        content_tag(:p, object.errors.full_messages_for(field_name).join(", "), class: "error-messages")
+      end
+    end
+  end
 end
