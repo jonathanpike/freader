a63612de5463cc062bef50c1933fd66a5ced2e77
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-13 14:38:28 +0000

    Add helper functions for stash button

--- app/helpers/stashes_helper.rb
+++ app/helpers/stashes_helper.rb
@@ -1,2 +1,11 @@
 module StashesHelper
+  include UtilityFunctions
+  
+  def stash_button_helper(article)
+    if stashed?(article.id)
+      link_to content_tag(:span, "Stashed!"), stash_path(Stash.where(article_id: article.id, user_id: current_user.id).first.id), method: :delete, remote: true, class: "button stash-delete", id: "stash_button_#{article.id}"
+    else 
+      render partial: "stashes/new_stash", locals: {article_id: article.id}
+    end 
+  end 
 end
