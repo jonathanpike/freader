15f5943f81cc693237e1c0ed66e754bdf8970251
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-13 14:39:43 +0000

    Clean up styles for subscription button

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -3,9 +3,9 @@ module SitesHelper
 
   def subscription_button_helper(site)
     if subscribed?(site.id)
-      link_to 'Subscribed!', subscription_path(site.id), method: :put, remote: true, class: "button"
+      link_to 'Subscribed!', subscription_path(site.id), method: :put, remote: true, class: "button button-outline"
     else
-      link_to 'Subscribe', subscription_path(site.id), method: :put, remote: true, class: "button"
+      link_to 'Subscribe', subscription_path(site.id), method: :put, remote: true, class: "button button-outline"
     end
   end
 end
