0a35cfd8889ded37dfe8bdaa2e2decd06efe9223
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-19 14:24:37 +0000

    Add 'time machine' features to go back and forward in your digest

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,12 +1,16 @@
 module SubscriptionsHelper
   def any_articles?(sites)
     sites.each do |site|
-      return true if site.articles.where("published >= ?", Time.zone.now.beginning_of_day).length > 0
+      return true if site.articles.where("published >= ?", range).length > 0
     end
     false
   end
 
   def updated_sites?(site)
-    site.articles.where("published >= ?", Time.zone.now.beginning_of_day).length > 0
+    site.articles.where("published >= ?", range).length > 0
   end
+  
+  def range
+    Time.parse(params[:date]) || Time.zone.now.beginning_of_day
+  end 
 end
