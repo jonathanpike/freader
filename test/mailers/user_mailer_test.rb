8e7e06edf1f446724a439ff6d81bb37dedcffa23
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-19 16:29:15 +0000

    Fix mailer tests

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,7 +6,7 @@ class UserMailerTest < ActionMailer::TestCase
     user.activation_token = User.new_token
     mail = UserMailer.activation_email(user)
     assert_equal "Activate your Account", mail.subject
-    assert_equal ["no-reply@freader.com"], mail.from
+    assert_equal ["no-reply@thedigest.io"], mail.from
     assert_equal [user.email], mail.to
     assert_match user.name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
@@ -18,7 +18,7 @@ class UserMailerTest < ActionMailer::TestCase
     user.reset_token = User.new_token
     mail = UserMailer.reset_email(user)
     assert_equal "Reset your Password", mail.subject
-    assert_equal ["no-reply@freader.com"], mail.from
+    assert_equal ["no-reply@thedigest.io"], mail.from
     assert_equal [user.email], mail.to
     assert_match user.name, mail.body.encoded
     assert_match user.reset_token, mail.body.encoded
