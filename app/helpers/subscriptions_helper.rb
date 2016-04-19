06bb885566e969c565a880fb92f98522835adf5a
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-19 14:48:20 +0000

    Limit range queried to 1 date instead of all entries published after certain date

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,20 +1,20 @@
 module SubscriptionsHelper
   def any_articles?(sites)
     sites.each do |site|
-      return true if site.articles.where("published >= ?", range).length > 0
+      return true if site.articles.where(published: range.beginning_of_day..range.end_of_day).length > 0
     end
     false
   end
 
   def updated_sites?(site)
-    site.articles.where("published >= ?", range).length > 0
+    site.articles.where(published: range.beginning_of_day..range.end_of_day).length > 0
   end
 
   def range
     if params[:date]
       Time.zone.parse(params[:date])
     else
-      Time.zone.now.beginning_of_day
+      Time.zone.now
     end
   end
 end
